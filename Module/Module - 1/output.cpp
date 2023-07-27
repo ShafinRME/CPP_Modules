c5de3c2cf250aaa3794237326d338663225d290c
Md.Safin Ahmed <shafin.rmedu@gmail.com>
2023-07-27 16:37:08 +0600

    cpp output is done

---
+++ Module/Module - 1/output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hellow "
+         << "World"
+         << "\n";
+    cout << "Hellow" << endl
+         << "World" << endl;
+    int a = 100, b = 202;
+    cout << a << " " << b << endl;
+    cout << a << endl
+         << b;
+
+    return 0;
+}
